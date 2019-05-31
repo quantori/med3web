167c3b82866367280745039c6bdf4eefd2d46f22
Vlad Shubnikov <vlad.shubnikov@gmail.com>
2019-05-31 13:09:10 +0300

    added react-googleapi
    added test to use google api

--- src/demo/App.js
+++ src/demo/App.js
@@ -12,6 +12,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { GoogleApi } from '@lourd/react-google-api';
+
 import UiApp from './ui/UiApp';
 
 import './App.css';
@@ -22,6 +24,8 @@ import './App.css';
 // Const
 // ********************************************************
 
+const NEED_GOOGLE_API = true;
+
 // ********************************************************
 // Class
 // ********************************************************
@@ -30,11 +34,53 @@ import './App.css';
  * Class App implements all application functionality. This is root class.
  */
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onApi = this.onApi.bind(this);
+  }
+  logObject(strTitle, obj) {
+    let str = '';
+    for (let prp in obj) {
+      if (str.length > 0) {
+        str += '\n';
+      }
+      str += prp + ' = ' + obj[prp];
+    }
+    console.log(`${strTitle}\n${str}`);
+  }
+  onApi(api) {
+    this.logObject('onApi with api = ', api)
+    const jsxOnApi = <p>
+      onApi invoked
+    </p>;
+    return jsxOnApi;
+  }
   /**
    * Main component render func callback
    */
   render() {
-    const jsxRender = <UiApp />;
+    if (!NEED_GOOGLE_API) {
+      return <UiApp />;
+    }
+    const googleApiKey = 'AIzaSyARCrLFJZcsKaadtyGK9mgzRBZqkopTR1s';
+    const idGoogle = 'shaped-plateau-242308.apps.googleusercontent.com';
+
+    const arrDocs = [
+      'Med3Web is the first and only dicom medical data web 2d/3d viewer'
+    ];
+    const arrScopes = [
+      'med3webApp'
+    ];
+
+    const jsxRender = <div>
+      <UiApp />
+      <p>
+        Test google api...
+      </p>
+      <GoogleApi clientId={idGoogle} apiKey={googleApiKey} discoveryDocs={arrDocs} scopes={arrScopes} children={this.onApi} >
+      </GoogleApi>
+      
+    </div>;
     return jsxRender;
   } // end render
 } // end class
